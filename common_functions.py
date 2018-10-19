a38fa2f22c5eb1605f6844b1f4ab5ae3f4332073
AndreaBarbon <andrea.3arbon@gmail.com>
2018-10-19 15:16:02 -0400

    Mac and Widnows

--- common_functions.py
+++ common_functions.py
@@ -1,41 +1,3 @@
-import pandas as pd; import numpy  as np; import os;
-
-### Stata regressions from python
-#
-## Dependencies for Stata: ivreg2 and reghdfe.
-## Run the following in Stata to install them:
-# ssc install reghdfe, replace
-# ssc install ivreg2, replace
-# ssc install ranktest, replace
-
-### CONFIGURATION ##############################
-#
-# Change this (if needed) to point to the Stata app:
-#
-# Mac:
-STATA_APP_MAC = "/Applications/Stata/StataMP.app/Contents/MacOS/StataMP"
-#
-# Windows:
-STATA_APP_WIN = "C:\Program Files (x86)\Stata14\StataMP-64.exe"
-#
-#
-# Set up the folder where outputs will be placed.
-# CAUTION: the folder name cannot contain white spaces
-TARGET_FOLDER = "Regressions"
-# Leave it `None` if you want everyhting to stay in the main working directory
-# If you change it, remember to save your .dta files in that folder
-#
-################################################
-
-if 'darwin' in platform: WINDOWS   = False; STATA_APP = STATA_APP_MAC
-else :                   WINDOWS   = True;  STATA_APP = STATA_APP_WIN
-
-if TARGET_FOLDER is None: TARGET_FOLDER = os.getcwd()
-slash = "\\" if WINDOWS else "/"
-    
-target = TARGET_FOLDER if (TARGET_FOLDER.endswith("/") | (TARGET_FOLDER=="")) else TARGET_FOLDER + slash
-if not target.startswith(os.getcwd()): target = os.getcwd() + slash + target
-
 ################################################
 # Functions
 
@@ -101,7 +63,6 @@ def write_do_file_for_regression(reg, specs=None, do_file_name=None, test_only=F
     
     if specs is None: specs = [reg]
         
-    sort = ""
     if 'sort' in reg.keys(): sort = " ".join(reg['sort'])
 
     all_fes = []
